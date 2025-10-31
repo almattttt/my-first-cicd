bd16152c4a4b6d1437acd7ba347315901e9bcff8
almattttt <almatag05@gmail.com>
2025-10-31 08:53:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Привет! 🚀 CI/CD работает!"
+    return "Привет! 🚀 новый CI/CD работает!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
